bf312990e829c03026e094c548cfd2a5024ac8d7
Emiliano <121838259+AeshEmi1@users.noreply.github.com>
2024-05-05 20:41:02 +0000

    Troubleshooting vlan_script.py

--- vlan_script.py
+++ vlan_script.py
@@ -9,12 +9,11 @@ class SwitchConfiguration:
             "username": username,
             "password": password
         }
-        print(switch_dict["host"])
-        try:
-            self.switch_connection = ConnectHandler(self.switch_dict)
-            pring(f"Connection successful to {switch_dict["host"]}!")
-        except Exception:
-            print("Connection failed! Maybe the switch is offline?")
+        #try:
+        self.switch_connection = ConnectHandler(self.switch_dict)
+        print(f"Connection successful to {switch_dict['host']}!")
+        #except Exception:
+        #    print("Connection failed! Maybe the switch is offline?")
 
 
     def identify_vlans(self):
@@ -35,23 +34,23 @@ class SwitchConfiguration:
 
 def main():
     # Read Ansible inventory file
-    try:
-        with open("/etc/ansible/inventory/switches", 'r') as f:
-            ansible_inventory = yaml.safe_load(f)
+#    try:
+    with open("/etc/ansible/inventory/switches", 'r') as f:
+        ansible_inventory = yaml.safe_load(f)
 
-            # Get switch ips and save them to an array
-            switch_ips = [host['ansible_host'] for host in ansible_inventory['switches']['hosts'].values()]
+        # Get switch ips and save them to an array
+        switch_ips = [host['ansible_host'] for host in ansible_inventory['switches']['hosts'].values()]
 
-            # Get the Switch's Credentials
-            username = ansible_inventory['switches']['vars']['ansible_user']
-            password = ansible_inventory['switches']['vars']['ansible_ssh_pass']
-            
-            # Create an array of SwitchConfiguration Objects
-            switches = [SwitchConfiguration(switch_ip, username, password) for switch_ip in switch_ips]
+        # Get the Switch's Credentials
+        username = ansible_inventory['switches']['vars']['ansible_user']
+        password = ansible_inventory['switches']['vars']['ansible_ssh_pass']
+        
+        # Create an array of SwitchConfiguration Objects
+        switches = [SwitchConfiguration(switch_ip, username, password) for switch_ip in switch_ips]
 
-            for switch in switches:
-                switch.identify_vlans()
-    except Exception:
-        print("Error reading the /etc/ansible/hosts file. Does it exist?")
+        for switch in switches:
+            switch.identify_vlans()
+#    except Exception:
+#        print("Error reading the /etc/ansible/hosts file. Does it exist?")
     
 main()
