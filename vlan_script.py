503a554ec1ee515a65e5bc2b2f1e70a9f5f03b9f
Emiliano <121838259+AeshEmi1@users.noreply.github.com>
2024-05-05 22:06:29 +0000

    Update vlan_script.py

--- vlan_script.py
+++ vlan_script.py
@@ -72,10 +72,10 @@ def main():
 
             # Add found vlans to the all_vlans set
             for switch in switches:
-                all_vlans.add(switch.identify_vlans())
+                all_vlans.update(switch.identify_vlans())
             
             # Print out the Vlans
-            print(f"VLANs: {", ".join(str(vlan) for vlan in all_vlans)}")
+            print(f"VLANs: {\", \".join(str(vlan) for vlan in all_vlans)}")
 
     except Exception as e:
         print(f"Error reading the /etc/ansible/inventory/switches file. Does it exist? - {e}")
